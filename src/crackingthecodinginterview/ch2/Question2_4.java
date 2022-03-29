edbc10181a12bd06db2bb26c1945094658a18a31
xingze.he <xingze.he@me2zen.com>
2022-03-29 12:38:52 +0800

    2-4 partition2:Don't need to find the final node of small list, nodeSmall is the final node of smallList

--- src/crackingthecodinginterview/ch2/Question2_4.java
+++ src/crackingthecodinginterview/ch2/Question2_4.java
@@ -64,10 +64,48 @@ public class Question2_4 {
     }
     // Time: O(N); Space: N
 
+
+    public ListNode partition2(ListNode head, int x) {
+        if (head == null) return null;
+        ListNode nodeBig = new ListNode(x);
+        ListNode nodeBigHead = nodeBig;
+        ListNode nodeSmall = new ListNode(x);
+        ListNode nodeSmallHead = nodeSmall;
+        while (head != null) {
+            // Add Smaller Ones to List1
+            if (head.val < x) {
+                nodeSmall.next = new ListNode(head.val);
+                nodeSmall = nodeSmall.next;
+            }
+            // Add Bigger Ones(or equal) to List2
+            else {
+                nodeBig.next = new ListNode(head.val);
+                nodeBig = nodeBig.next;
+            }
+            head = head.next;
+        }
+        // Add List1+List2
+        nodeSmallHead = nodeSmallHead.next;
+        nodeBigHead = nodeBigHead.next;
+        // Don't need to find the final node of small list, nodeSmall is the final node of smallList
+        if (nodeSmallHead != null) {
+            nodeSmall.next = nodeBigHead;
+            return nodeSmallHead;
+        }
+        else {
+            return nodeBigHead;
+        }
+
+    }
+    // Time: O(N); Space: N
+
+
     public static void main(String[] args) {
         ListNode list1 = new ListNode(1);
+//        list1.next = new ListNode(5);
+//        list1.next.next = new ListNode(3);
         Question2_4 q = new Question2_4();
-        ListNode list2 = q.partition1(list1, 0);
+        ListNode list2 = q.partition2(list1, 0);
         System.out.println(list2);
     }
 
