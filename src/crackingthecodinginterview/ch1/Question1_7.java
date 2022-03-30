dbaa675bc8c9768cf0a8d8eff4fb135ce0faafa5
Michael <michaelzbj2019@outlook.com>
2022-03-30 10:51:48 +0800

    Q1_7 Initial

--- src/crackingthecodinginterview/ch1/Question1_7.java
+++ src/crackingthecodinginterview/ch1/Question1_7.java
@@ -66,6 +66,12 @@ public class Question1_7 {
         return index;
     }
 
+    public void rotate1(int[][] matrix) {
+
+
+
+    }
+
     public static void main(String[] args) {
         Question1_7 q = new Question1_7();
         int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
